c0308bd2b3166e00e5852ad01b7fd14ee472d27d
magikcypress <cyp@rouquin.me>
2022-08-06 19:41:32 +0200

    Create dist folder

---
+++ docs/.vuepress/dist/assets/Japon.html.2ef19978.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-5c96667d","path":"/travel/Japon.html","title":"Japan","lang":"en-US","frontmatter":{"sidebar":false,"prev":"./Canada","next":false},"excerpt":"","headers":[],"git":{"updatedTime":1659800329000,"contributors":[{"name":"magikcypress","email":"cyp@rouquin.me","commits":2}]},"filePathRelative":"travel/Japon.md"}');export{e as data};
