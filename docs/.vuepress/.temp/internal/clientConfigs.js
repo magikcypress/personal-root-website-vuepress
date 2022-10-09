504b49aef80f7996ea372b5a04b2c7ba336a5a88
magikcypress <cyp@rouquin.me>
2022-10-09 16:50:16 +0200

    Update config & package

--- docs/.vuepress/.temp/internal/clientConfigs.js
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -1,9 +1,9 @@
 import clientConfig0 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
 import clientConfig1 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-external-link-icon/lib/client/config.js'
-import clientConfig2 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
-import clientConfig3 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/theme-default/lib/client/config.js'
-import clientConfig4 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
-import clientConfig5 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import clientConfig2 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import clientConfig3 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import clientConfig4 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/theme-default/lib/client/config.js'
+import clientConfig5 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
 import clientConfig6 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/vuepress-plugin-chart/lib/client/config.js'
 import clientConfig7 from '/mnt/c/Users/veg/F/web/personal-root-website-vuepress/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
 
--- docs/.vuepress/.temp/pages/fr/travel/Vietnam.html.js
+++ docs/.vuepress/.temp/pages/fr/travel/Vietnam.html.js
@@ -1,1 +1,14 @@
 export const data = JSON.parse("{\"key\":\"v-54061e0c\",\"path\":\"/fr/travel/Vietnam.html\",\"title\":\"Vietnam\",\"lang\":\"Français\",\"frontmatter\":{\"sidebar\":false,\"prev\":\"./Maroc\",\"next\":\"./Bresil\"},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":1659800329000,\"contributors\":[{\"name\":\"magikcypress\",\"email\":\"cyp@rouquin.me\",\"commits\":2}]},\"filePathRelative\":\"fr/travel/Vietnam.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
